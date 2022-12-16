95094be6f1b2ae796570984f85f4f66defb8af70
laxshanpanchu <108828860+laxshanpanchu@users.noreply.github.com>
2022-12-16 22:43:22 +0530

    added admin part to main project and committing to laxshan branch

---
+++ admin/admin_changepassword.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar">
+
+<div class="logo">
+<img src="./image/logo.png"/>
+ </div>
+
+<div class="profile">
+    <img src="./image/profile.png"/>
+</div>
+
+    <div class="home">
+        <a href="asst_home.php">
+    <label>Home</label>
+        </a>
+    </div>
+
+    <div class="session">
+        <a href="asst_session.php">
+    <label>Sessions</label>
+        </a>
+    </div>
+
+    <div class="view-patient">
+        <a href="#">
+    <label>View patient</label>
+        </a>
+    </div>
+
+
+    <div class="view-order">
+        <a href="#">
+    <label>View orders</label>
+        </a>
+    </div>
+
+    
+    <div class="view-appointment">
+        <a href="#">
+    <label>View Appointments</label>
+        </a>
+    </div>
+
+    <div class="report">
+        <a href="#">
+    <label>Reports</label>
+        </a>
+    </div>
+
+    
+    <div class="system-user">
+        <a href="#">
+    <label>System users</label>
+        </a>
+    </div>
+
+
+    <div class="view-profile">
+        <a href="asst_view.php">
+    <label>View Profile</label>
+        </a>
+    </div>
+
+
+    <div class="signout">
+    
+        <a href="#">
+            <label>Sign Out</label>   
+        </a>
+   </div>
+</div>
+<!-- navgation-->
+
+
+<div class="change_profile">
+     Change password
+  </div>
+<form >
+
+   <div class="oldpassword-1">
+        <label for="oldpassword" >Old password: </lable>
+            <input id="oldpassword" type="text" >
+    </div>
+
+        
+
+
+    <div class="newpassword-1">
+        <label for="newpassword" >New password: </lable>
+            <input id="newpassowrd" type="text" >
+    </div>
+
+    <div class="conformpassword-1">
+        <label for="conformpassword" >Conform password: </lable>
+            <input id="conformpassword" type="text" >
+    </div>
+
+</form>
+
+<a href="./signin.php" class="nav-signin"><div class="item5 active-btn">Sign In</div></a>
+<div class="divider-save">
+    <button type="button">Save</button>
+</div>
+<div class="divider-back">
+    <button type="button">Back</button>
+</div>
+
+</body>
+</html>
---
+++ admin/admin_home.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>admin-home</title>
+</head>
+<body>
+    <div class="navbar">
+
+        <div class="logo">
+        <img src="./image/logo.png"/>
+         </div>
+
+        <div class="profile">
+            <img src="./image/profile.png"/>
+        </div>
+
+            <div class="home">
+                <a href="asst_home.php">
+            <label>Home</label>
+                </a>
+            </div>
+        
+            <div class="session">
+                <a href="asst_session.php">
+            <label>Sessions</label>
+                </a>
+            </div>
+
+            <div class="view-patient">
+                <a href="#">
+            <label>View patient</label>
+                </a>
+            </div>
+
+
+            <div class="view-order">
+                <a href="#">
+            <label>View orders</label>
+                </a>
+            </div>
+
+            
+            <div class="view-appointment">
+                <a href="#">
+            <label>View Appointments</label>
+                </a>
+            </div>
+
+            <div class="report">
+                <a href="#">
+            <label>Reports</label>
+                </a>
+            </div>
+
+            
+            <div class="system-user">
+                <a href="#">
+            <label>System users</label>
+                </a>
+            </div>
+        
+        
+            <div class="view-profile">
+                <a href="asst_view.php">
+            <label>View Profile</label>
+                </a>
+            </div>
+    
+
+            <div class="signout">
+            
+                <a href="#">
+                    <label>Sign Out</label>   
+                </a>
+           </div>
+        </div>
+
+<!-- navgation-->
+
+<div class="welcome">
+ Welcome Laxshan
+</div>
+
+<div class="box-1" style="font-family: 'Poppins';">
+ No of appointments
+ <div class="in-box-1" style="font-family: 'Poppins';">230</div>
+</div>
+
+ <div class="box-2" style="font-family: 'Poppins';">
+ No of sessions
+ <div class="in-box-2" style="font-family: 'Poppins';">20</div>
+</div>
+ <div class="box-3" style="font-family: 'Poppins';">
+  Canceled sessions<br> for this week
+  <div class="in-box-3" style="font-family: 'Poppins';">1</div>
+</div>
+ <div class="box-4" style="font-family: 'Poppins';">
+  Conformed sessions<br> for this week
+  <div class="in-box-4" style="font-family: 'Poppins';">4</div>
+</div>
+</body>
+</html>

8af7b43dc77105fb17381b93a7e55c86766e47b6
saiNan24 <118580513+saiNan24@users.noreply.github.com>
2022-12-17 06:51:45 +0530

    started creating patient home

---
+++ patient/patient_home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/patient.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <a href="../index.php"><img src="../images/logo.png" alt="logo" class="logo"></a>
+            <img src="../images/user.png" alt="user" class="imgframe">
+            <ul>
+                <li><a href="patient_home.php">Home</a></li>
+                <li><a href="lab_appointments.php">Appointments</a></li>
+                <li><a href="lab_appointments.php">Orders</a></li>
+                <li><a href="lab_appointments.php">Medical records</a></li>
+                <li><a href="lab_appointments.php">Lab reports</a></li>
+                <li><a href="lab_viewprofile.php">View Profile</a></li>
+            </ul>
+            <div class="signouttext"><a href="#">Sign Out</a></div>
+        </div>
+        <div class="right_content">
+            <div class="welcome-text">
+                <p>Welcome</p><p class="coloured-name">Laxshan</p>
+            </div>
+        </div>
+
+
+    </div>
+
+
+</body>
+
+</html>

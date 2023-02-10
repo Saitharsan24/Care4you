24338329c0050730bba067895b7b91955accf58d
saiNan24 <118580513+saiNan24@users.noreply.github.com>
2023-02-10 05:42:31 +0530

    Updated patient. Changed html to php.

--- patient/patient_home.php
+++ patient/patient_home.php
@@ -1,5 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+<<<<<<< Updated upstream
+=======
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/patient.css" />
+    <title>Home</title>
+  </head>
+  <body>
+    <div class="main-div">
+      <div class="home-left">
+        <div class="nav-logo">
+          <a href="./patient_home.php">
+            <img src="../images/logo.png" alt="logo" />
+          </a>
+        </div>
+        <div class="profile-image">
+          <img src="../images/user.png" alt="profile-image" />
+        </div>
+        <div class="nav-links">
+          <a href="./patient_home.php" style="color: #0c5c75; font-weight: bold">Home</a>
+          <a href="./patient_appointments.php">Appointments</a>
+          <a href="./patient_pharmorders.php">Orders</a>
+          <a href="./patient_medicalrecords.php">Medical records</a>
+          <a href="#">View doctors</a>
+          <a href="#">View profile</a>
+        </div>
+        <div class="signout"><a href="../logout.php">Sign Out</a></div>
+      </div>
+      <div class="home-right">
+        <div class="text-content">
+          Welcome
+          <div class="user-name">Name</div>
+        </div>
+        <?php 
+                if(isset($_SESSION['login'])){
+                    echo $_SESSION['login'];
+                    unset($_SESSION['login']);
+>>>>>>> Stashed changes
 
 <head>
     <meta charset="UTF-8">
@@ -9,6 +49,7 @@
     <title>Home | Patient</title>
 </head>
 
+<<<<<<< Updated upstream
 <body>
     <div class="wrapper">
         <div class="sidebar">
@@ -44,6 +85,21 @@
                     <button class="btn-blue1"><a href="pharmacy_vieworder.php">View Order</a></button>
                 </div>
             </div>
+=======
+                }
+            ?>
+        <div class="page-content">
+          <div class="left-page-content">
+           
+          </div>
+          <div class="right-page-content">
+            <div><a href="./patient_makedocappointment.php"><button>Make doctor appointment</button></a></div>
+            <div class="home-divider"></div>
+            <div><a href="./patient_makeorder.php"><button>Order medicine</button></a></div>
+            <div class="home-divider"></div>
+            <div><a href="./patient_makelabappointment.php"><button>Make lab appointment</button></a></div>
+          </div>
+>>>>>>> Stashed changes
         </div>
     </div>
      
---
+++ patient/patient_viewdocappointment.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/patient.css" />
+    <title>Home</title>
+  </head>
+  <body>
+    <div class="main-div">
+      <div class="home-left">
+        <div class="nav-logo">
+          <a href="./patient_home.php">
+            <img src="../images/logo.png" alt="logo" />
+          </a>
+        </div>
+        <div class="profile-image">
+          <img src="../images/user.png" alt="profile-image" />
+        </div>
+        <div class="nav-links">
+          <a href="./patient_home.php">Home</a>
+          <a href="./" style="color: #0c5c75; font-weight: bold">Appointments</a>
+          <a href="./patient_pharmorders.php">Orders</a>
+          <a href="#">Medical records</a>
+          <a href="./patient_medicalrecords.php">View doctors</a>
+          <a href="#">View profile</a>
+        </div>
+        <div class="signout"><a href="#">Sign Out</a></div>
+      </div>
+      <div class="home-right">
+        <div class="view-apt-heading"><h2>My Appointments</h2></div>
+        <div class="view-apt-details">
+          <div class="view-details-row"><p>Reference No :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>Doctor Name :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>Date :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>Appointment No :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>Appointment time :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>Room No :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>Patient name :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>NIC No :</p><div><p></p></div></div>
+          <div class="view-details-row"><p>Total Amount :</p><div><p></p></div></div>
+          <div class="view-apt-btn">
+            <div class="view-apt-btn01"><a href=""><button>Cancel appointment</button></a></div>
+            <div class="view-apt-divider"></div>
+            <div class="view-apt-btn02"><a href=""><button>Pay now</button></a></div>
+          </div>
+        </div>
+        <div class="view-apt-back-btn">
+          <div class="view-apt-btn02"><a href="./patient_docappointments.php"><button>Back</button></a></div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>

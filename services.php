96c16a99735f992c8dd9302bfe94b68c2e4fa7a4
Dewmini Devindya <120557607+dewminidevindya@users.noreply.github.com>
2023-03-01 19:46:41 +0530

    CSS Changes

--- services.php
+++ services.php
@@ -5,23 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css"> 
-    <title>Services</title>
+    <title>What We Serve</title>
+    <link rel="icon" type="images/x-icon" href="./images/logoicon.png" />
+    <script src="https://kit.fontawesome.com/ca1b4f4960.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="navbar">
-        <a href="./index.php"><img src="./images/logo.png" alt="logo" class="logo"></a>
-        <div class="nav-link">
-            <div class="normal-link">
-                <div class="nav-item item1"><a href="./index.php">Home</a></div>
-                <div class="nav-item item2"><a href="./services.php" class="active-link">Services</a></div>
-                <div class="nav-item item3"><a href="./about.php">About</a></div>
-                <div class="nav-item item4"><a href="./contactus.php">Contact Us</a></div>   
-            </div>  
-            <a href="./signin.php" class="nav-signin"><div class="item5">Sign In</div></a>
-            <div class="divider"></div>
-            <a href="signup.php" class="nav-signin"><div class="item6">Sign Up</div></a>
+    <div class="top">
+        <div class="navbar">
+            <a href="./index.php"><img src="./images/logo.png" alt="logo" class="logo"></a>
+            <div class="nav-link">
+                <div class="normal-link">
+                    <div class="nav-item item1"><a href="./index.php" class="active-link">Home</a></div>
+                    <div class="nav-item item2"><a href="./services.php">Services</a></div>
+                    <div class="nav-item item3"><a href="./about.php">About</a></div>
+                    <div class="nav-item item4"><a href="./contactus.php">Contact Us</a></div>   
+                </div>  
+                <a href="./signin.php" class="nav-signin"><div class="item5"><i class="fa-solid fa-right-from-bracket"></i> &nbsp; Sign In</div></a>
+                <div class="divider"></div>
+                <a href="./signup.php" class="nav-signin"><div class="item5"><i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Sign Up</div></a>
+            </div>
         </div>
     </div>
-
+    <div class="bottom">
+        
+    </div>
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -6,70 +6,73 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css"> 
-    <title>SignIn</title>
+    <title>Sign In</title>
+    <link rel="icon" type="images/x-icon" href="./images/logoicon.png" />
+    <script src="https://kit.fontawesome.com/ca1b4f4960.js" crossorigin="anonymous"></script>
 </head>
 <body>
-
-    <!-- Nav bar -->
-    <div class="navbar">
-        <a href="./index.php"><img src="./images/logo.png" alt="logo" class="logo"></a>
-        <div class="nav-link">
-            <div class="normal-link">
-                <div class="nav-item item1 active-home"><a href="./index.php">Home</a></div>
-                <div class="nav-item item2"><a href="./services.php">Services</a></div>
-                <div class="nav-item item3"><a href="./about.php">About</a></div>
-                <div class="nav-item item4"><a href="./contactus.php">Contact Us</a></div>   
-            </div>  
-            <a href="./signin.php" class="nav-signin"><div class="item5 active-btn">Sign In</div></a>
-            <div class="divider"></div>
-            <a href="./signup.php" class="nav-signin"><div class="item6">Sign Up</div></a>
+    <div class="top">
+        <div class="navbar">
+            <a href="./index.php"><img src="./images/logo.png" alt="logo" class="logo"></a>
+            <div class="nav-link">
+                <div class="normal-link">
+                    <div class="nav-item item1"><a href="./index.php" class="active-link">Home</a></div>
+                    <div class="nav-item item2"><a href="./services.php">Services</a></div>
+                    <div class="nav-item item3"><a href="./about.php">About</a></div>
+                    <div class="nav-item item4"><a href="./contactus.php">Contact Us</a></div>   
+                </div>  
+                <a href="./signin.php" class="nav-signin"><div class="item5"><i class="fa-solid fa-right-from-bracket"></i> &nbsp; Sign In</div></a>
+                <div class="divider"></div>
+                <a href="./signup.php" class="nav-signin"><div class="item5"><i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Sign Up</div></a>
+            </div>
         </div>
     </div>
+    <div class="bottom">
+        <div class="wrapper">
+        <div class= "container">
+        <div class="container_content">
+        <div class="container_content_inner">
 
-    <!-- Sign in form -->
-    <div class="wrapper">
-    <div class= "container">
-    <div class="container_content">
-    <div class="container_content_inner">
-        <form class="form-signin" action="#" method="POST">       
-            <h2 class="form-signin-heading">Sign In</h2>
-            <?php 
-                if(isset($_SESSION['login']))
-                {
-                    echo $_SESSION['login'];
-                    unset($_SESSION['login']);
-                }
-                if(isset($_SESSION['no-login-message']))
-                {
-                    echo $_SESSION['no-login-message'];
-                    unset($_SESSION['no-login-message']);
+            <form class="form-signin" action="#" method="POST">       
+                <h2 class="form-signin-heading">Sign In</h2>
+                <?php 
+                    if(isset($_SESSION['login']))
+                    {
+                        echo $_SESSION['login'];
+                        unset($_SESSION['login']);
+                    }
+                    if(isset($_SESSION['no-login-message']))
+                    {
+                        echo $_SESSION['no-login-message'];
+                        unset($_SESSION['no-login-message']);
 
-                }
-            ?>
-            <div class="par">
-            <p class="form-signin-username">User Name</p>
-            <input type="text" class="form-control" name="username" placeholder="Enter username here" required="" autofocus="true"/><br />
-            
-            <p class="form-signin-password">Password</p>
-            <input type="password" class="form-control" name="password" placeholder="Enter password here" required=""/>
+                    }
+                ?>
+                <div class="par">
+                <p class="form-signin-username">User Name</p>
+                <input type="text" class="form-control" name="username" placeholder="Enter username here" required="" autofocus="true"/><br />
+                
+                <p class="form-signin-password">Password</p>
+                <input type="password" class="form-control" name="password" placeholder="Enter password here" required=""/>
 
-            <div  class="forgot-password">
-                <a href="" class="forgot-password">Forgot password?</a>
-            </div>
+                <div  class="forgot-password">
+                    <a href="" class="forgot-password">Forgot password?</a>
+                </div>
 
-            <br />
-            <br />
+                <br />
+                <br />
 
-            <a href="signup.php" class="donothave-account">Do not have an account?</a>  
+                <a href="signup.php" class="donothave-account">Do not have an account?</a>  
 
-            <button class="btn signin" name="submit" type="submit">Sign In</button>
-            </div>   
-        </form>
-    </div> 
-    </div> 
-    </div> 
+                <button class="btn signin" name="submit" type="submit">Sign In</button>
+                </div>   
+            </form>
+        </div> 
+        </div> 
+        </div> 
+        </div>
+        <div class="overlay"></div>
     </div>
-    <div class="overlay"></div>
 </body>
 </html>
 

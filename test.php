20e9d910a834cd357c91dee01eebbd65bc775524
Dewmini Devindya <120557607+dewminidevindya@users.noreply.github.com>
2023-04-27 05:02:03 +0530

    CSS - Doctor Reg.

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css"> 
+    <title>About Us</title>
+    <link rel="icon" type="images/x-icon" href="./images/logoicon.png" />
+    <script src="https://kit.fontawesome.com/ca1b4f4960.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <button onclick="openForm()" class="nav-signin"><div class="item5"><i class="fa-solid fa-arrow-up-from-bracket"></i> &nbsp; Sign Up</div></button>
+    <?php include('signupp.php') ?>
+
+</body>
+</html>

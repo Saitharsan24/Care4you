dffce2c0f696c019d5a7eba543736a5332183221
Dewmini Devindya <120557607+dewminidevindya@users.noreply.github.com>
2023-05-04 18:36:56 +0530

    Popup , Admin(patients)

--- admin/admin-patient-view.php
+++ admin/admin-patient-view.php
@@ -20,7 +20,9 @@ $no_row = mysqli_num_rows($result);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/admin-view-patient.css">
+    <link rel="stylesheet" href="../css/admin.css">
     <title>ADMIN</title>
+    <link rel="icon" type="images/x-icon" href="../images/logoicon.png" />
     <script src="https://kit.fontawesome.com/ca1b4f4960.js" crossorigin="anonymous"></script>
 </head>
 
@@ -32,28 +34,25 @@ $no_row = mysqli_num_rows($result);
             <ul>
                 <li><a href="admin_home.php">Home</a></li>
                 <li><a href="admin-session-view.php">Sessions</a></li>
-                <li><a href="admin-patient-view.php">Patients</a></li>
+                <li><a href="admin-patient-view.php"><div class="highlighttext">Patients</div></a></li>
                 <li><a href="#">Orders</a></li>
                 <li><a href="#">Appointments</a></li>
                 <li><a href="#">Reports</a></li>
-                <li><a href="admin-system-users.php"><div class="highlighttext">System Users</div></a></li>
+                <li><a href="admin-system-users.php">System Users</a></li>
                 <li><a href="admin_viewprofile.php">Profile</a></li>
             </ul>
             <div class="signouttext"><a href="../logout.php"><i class="fa-solid fa-right-from-bracket"></i> Sign Out </a></div>
 
             <div class="main_content">
                 <div class="info">
-                  
-                    <div class="view-patient-txt">View Patients </div>
-                  
 
                     <span>
-                        <table class="tbl-main-patient">
+                        <table class="tbl-main-patient" style="margin-top:-50px;">
                             <thead>
                                 <tr>
                                     <td>Patient ID</td>
-                                    <td>Full Name</td>
-                                    <td>Phone Number</td>
+                                    <td>Patient Name</td>
+                                    <td>Contact Number</td>
                                     <td>Account Status</td>
                                     <td>View Details</td>
                                 </tr>
@@ -64,7 +63,7 @@ $no_row = mysqli_num_rows($result);
                                         <td><input type="text" class="search-patient" name="patient-name" autofocus="true" /></td>
                                         <td><input type="text" class="search-patient" name="phone-no" autofocus="true" /></td>
                                         <td><input type="text" class="search-patient" name="status" autofocus="true" /></td>
-                                        <td><button class="btn-view-patient-detail"><span>Search</span></button></td>
+                                        <td><button class="btn-search"><span>Search&emsp;</span></button></td>
                                     </tr>
                                     <?php 
                                    if($result){
@@ -79,7 +78,7 @@ $no_row = mysqli_num_rows($result);
                                                
                                                 <?php
                                                 if ($row['status'] == 1) { 
-                                                    echo '<button class="active-status"> Active </button>';
+                                                    echo '<button class="active-status">Active</button>';
                                                 } else {
                                                     echo '<button class="passive-status"> Disabled </button>';
                                                 }

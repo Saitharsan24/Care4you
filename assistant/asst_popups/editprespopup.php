71f9c5b3c122db5e2776df94ca02371e8b1ba99a
Dewmini Devindya <120557607+dewminidevindya@users.noreply.github.com>
2023-05-13 13:34:02 +0530

    Update editprespopup.php

--- assistant/asst_popups/editprespopup.php
+++ assistant/asst_popups/editprespopup.php
@@ -308,15 +308,15 @@ section.active .modal-box {
 <section id="editpop">
   <span class="overlay" onclick="closePopupE()"></span>
 
-  <div class="modal-box" style="text-align: center; justify-content: center;align-items: center;">
+  <div class="modal-box" style="text-align: center; justify-content: center;align-items: center; height:65%;">
     <i class="fa-solid fa-pen-to-square" style="color: #0d5c75;"></i> <br/>
 
     <form type="submit">
-    <table>
+    <table class="viewtbl" style="width:95%;">
         
         <tr>
-            <td class="typeR">Appointment Status :</td>
-            <td class="typeL">
+            <td class="typeR" style="width:50%; font-size:15px;">Appointment Status :</td>
+            <td class="typeL" style="width:50%; font-size:15px;">
             
                 <label class="switch">
                     <input type="checkbox" id="toggle" name="attendance">
@@ -339,8 +339,8 @@ section.active .modal-box {
         </tr>
 
         <tr>
-            <td class="typeR">Upload Prescription :</td>
-                <td class="typeL">
+            <td class="typeR" style="width:50%; font-size:15px;">Upload Prescription :</td>
+                <td class="typeL" style="width:50%; font-size:15px;">
                 <form method="POST">  
                 <div class="type-file upload-input">
                     <input type="file" accept="image/*,.doc,.docx,.txt,.pdf" name="prescription"/>
@@ -349,8 +349,8 @@ section.active .modal-box {
         </tr>
 
         <tr>
-            <td class="typeR">Other Remarks :</td>
-            <td class="typeL">
+            <td class="typeR" style="width:50%; font-size:15px;">Other Remarks :</td>
+            <td class="typeL" style="width:50%; font-size:15px;">
                 <form method="POST">
                     <textarea class="textarea" name="asst-remark" id="address" placeholder="Enter Doctor Remarks Here"></textarea>
             </td>

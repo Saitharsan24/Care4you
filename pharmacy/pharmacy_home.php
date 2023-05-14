e6b207664f657ff133cd8a58ba26db1d41e3dae3
Dewmini Devindya <120557607+dewminidevindya@users.noreply.github.com>
2023-05-14 23:20:54 +0530

    Pharmacy Home UI

--- pharmacy/pharmacy_home.php
+++ pharmacy/pharmacy_home.php
@@ -2,6 +2,7 @@
 <?php include('../login_access.php') ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,89 +12,63 @@
     <link rel="icon" type="images/x-icon" href="../images/logoicon.png" />
     <script src="https://kit.fontawesome.com/ca1b4f4960.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-<?php include('pharmacy_getinfo.php') ?>
+    <?php include('pharmacy_getinfo.php') ?>
     <div class="wrapper">
         <div class="sidebar">
             <a href="../index.php"><img src="../images/logo.png" alt="logo" class="logo"></a>
             <img src="../images/user-profilepic/pharmacist/<?php echo $profile_picture; ?>" alt="user" class="imgframe">
             <ul>
-                <li><a href="pharmacy_home.php"><div class="highlighttext">Home</div></a></li>
+                <li><a href="pharmacy_home.php">
+                        <div class="highlighttext">Home</div>
+                    </a></li>
                 <li><a href="pharmacy_neworders.php">New Orders</a></li>
                 <li><a href="pharmacy_orderhistory.php">Order History</a></li>
                 <li><a href="pharmacy_stock.php">Drug Stock</a></li>
                 <li><a href="pharmacy_viewprofile.php">Profile</a></li>
             </ul>
-            <div class="signouttext"><a href="../logout.php"><i class="fa-solid fa-right-from-bracket"></i> Sign Out </a></div>
+            <div class="signouttext"><a href="../logout.php"><i class="fa-solid fa-right-from-bracket"></i> Sign Out
+                </a></div>
         </div>
-        <div class="main_content"> 
+        <div class="main_content">
             <div class="info">
-            <?php 
-                if(isset($_SESSION['login'])){
+                <?php
+                if (isset($_SESSION['login'])) {
                     echo $_SESSION['login'];
                     unset($_SESSION['login']);
 
                 }
-                if(isset($_SESSION['no-login-message'])){
+                if (isset($_SESSION['no-login-message'])) {
                     echo $_SESSION['no-login-message'];
                     unset($_SESSION['no-login-message']);
 
                 }
-            ?>
-            <div class="welcometext">Welcome <div class="usernametext">
-                <?php echo $_SESSION['user']; ?>
-            </div>
-            </div>
-            <span>
-            <table class="center">
-                <tr>
-                    <td>
-                        <p id="rcorners">
-                            New Orders
-                            </br></br></br>
-                            <span style="color:#0D5C75; font-size: 70px; font-weight: 700;">
-                            <!-- 10 -->
-                            <?php
-                                //Query to get all data from tbl_neworder table
-                                $sql = "SELECT * FROM tbl_neworder";
-                                //Exeute the Query                                    
-                                $res = mysqli_query($conn, $sql);
-
-                                //Check Query executed or not
-                                if($res == TRUE)
-                                {
-                                    $count = mysqli_num_rows($res);
-                                    echo $count;
-                                }
-                            ?>
-                            </span>
-                        </p>
-                    </td>
-                    <td>
-                        <p id="rcorners">
-                            Pending Payments
-                            </br></br></br>
-                            <span style="color:#0D5C75; font-size: 70px; font-weight: 700;">
-                            23
-                            </span>
-                        </p>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan=2 style="padding-left: 435px; padding-top: 50px;">
-                        <p id="rcorners">
-                            To be Delivered
-                            </br></br></br>
-                            <span style="color:#0D5C75; font-size: 70px; font-weight: 700;">
-                            12
-                            </span>
-                        </p>
-                    </td>
-                </tr>
-            </table>
-            </span>
+                ?>
+                <div class="welcometext">Welcome <div class="usernametext">
+                        <?php echo $_SESSION['user']; ?>
+                    </div>
+                </div>
+                <div>
+                    <div class="flex-cont">
+                        <div class="graph-cont">
+                            <<< Graph Here >>>
+                        </div>
+                        <div class="box-cont">
+                            <div class="box-sub">
+                                <div class="box-title"> New Orders </div>
+                                <div class="box-data"> 05 </div>
+                            </div>
+                            <div class="box-sub" style="margin-top: 15%;">
+                                <div class="box-title"> Orders Completed </div>
+                                <div class="box-data"> 10 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
